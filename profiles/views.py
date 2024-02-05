f7377c1b921513a39274fa33b2a23b84701373f7
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-02-05 19:40:53 +0100

    ProfileList view created and added to the url path

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.db.models import Count
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from config.permissions import IsOwnerOrReadOnly
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileList(generics.ListAPIView):
+    """
+    List all profiles.
+    
+    owner = username of the profile owner
+    is_owner = true if the user is the owner of the profile
+    """
+    
+    queryset = Profile.objects.all()
+    
+    serializer_class = ProfileSerializer
