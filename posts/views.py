31d89616f79151b26ff6734e787120c0b41e2c19
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-02-06 00:21:17 +0100

    Create PostDetail view and add it to the url path

--- posts/views.py
+++ posts/views.py
@@ -13,10 +13,6 @@ class PostList(generics.ListCreateAPIView):
     
     owner = username of the post owner
     is_owner = true if the user is the owner of the post
-    profile_id = id of the profile of the post owner
-    profile_image = url of the profile image of the post owner
-    created_at = natural time or date of the post creation
-    updated_at = natural time or date of the post update
     """
     
     queryset = Post.objects.all()
@@ -27,3 +23,18 @@ class PostList(generics.ListCreateAPIView):
     
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a post.
+    
+    owner = username of the post owner
+    is_owner = true if the user is the owner of the post
+    """
+    
+    permission_classes = [IsOwnerOrReadOnly]
+    
+    queryset = Post.objects.all()
+    
+    serializer_class = PostSerializer
