5ded1eb963a892ebbcc3cebf0fa7414ad62605e4
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-02-05 20:11:24 +0100

    Created Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Post model for storing blog posts
+    """
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    audio = models.FileField(blank=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(
+        upload_to='post_images/',
+        null=True,
+        blank=True,
+        default=''
+    )
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.id} {self.title}"
