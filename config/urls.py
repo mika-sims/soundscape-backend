bb30d9e37c635f57e6924a5b567e9fac013e3aa6
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-02-06 00:02:42 +0100

    Add post urls to path

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('soundscape/api/', include('profiles.urls')),
+    path('soundscape/api/', include('posts.urls')),
 ]
