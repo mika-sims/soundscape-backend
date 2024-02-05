e213f49f44ce21935ee1b2bf9f92aae2929e0a76
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-02-05 19:01:35 +0100

    Import profile signal and register it

--- profiles/apps.py
+++ profiles/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'
+
+    def ready(self):
+        # Import the profile signals
+        import profiles.signals
