0cfa349b5934b92049558d7e906ff69ec5396ff8
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-02-06 15:10:48 +0100

    Create LikeList view and add to url path and migrate the List model

--- likes/urls.py
+++ likes/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import LikeList
+
+urlpatterns = [
+    path('likes/', LikeList.as_view())
+]
--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+
+from .models import Like
+from .serializers import LikeSerializer
+
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    View to list all likes or create a new like.
+    Only authenticated users can create likes.
+    """
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
