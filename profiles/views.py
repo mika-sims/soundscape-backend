7edad922e85f55310b0023beaa86cca0c8a1bcec
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-02-06 22:40:29 +0100

    Add followers_count and following_count fields to queryset, ordering and filterset fields

--- profiles/views.py
+++ profiles/views.py
@@ -16,6 +16,8 @@ class ProfileList(generics.ListAPIView):
 
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
     filter_backends = [
@@ -24,6 +26,14 @@ class ProfileList(generics.ListAPIView):
     ]
     ordering_fields = [
         'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
     ]
 
 
@@ -38,7 +48,7 @@ class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
-
-
