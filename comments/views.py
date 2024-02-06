f08e5819125ff4422038c3fca5d6ffefcb18196e
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-02-06 14:29:00 +0100

    Create CommentDetail view

--- comments/views.py
+++ comments/views.py
@@ -2,13 +2,17 @@ from rest_framework import generics, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Comment
-from .serializers import CommentSerializer
+from .serializers import CommentSerializer, CommentDetailSerializer
+from config.permissions import IsOwnerOrReadOnly
 
 
 class CommentList(generics.ListCreateAPIView):
     """
     View for retrieving a list of comments or 
     creating a new comment if the user is authenticated
+
+    owner = username of the post owner
+    is_owner = true if the user is the owner of the post
     """
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -18,3 +22,12 @@ class CommentList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    View for retrieving, updating or deleting a comment
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
