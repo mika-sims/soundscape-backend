86c82e35cff30cc7b9ab390940aa40161f2ddeb8
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-02-06 14:29:30 +0100

    Add CommentDetail view to url path

--- comments/urls.py
+++ comments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CommentList
+from .views import CommentList, CommentDetail
 
 
 urlpatterns = [
     path('comments/', CommentList.as_view()),
+    path('comments/<int:pk>/', CommentDetail.as_view())
 ]
