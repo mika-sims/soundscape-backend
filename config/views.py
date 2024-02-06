e3fb4c5e004d819f4fbe40e05d5f6d54aa8a7998
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-02-06 01:19:42 +0100

    Delete unused import

--- config/views.py
+++ config/views.py
@@ -1,6 +1,5 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
 from .settings import (
     JWT_AUTH_COOKIE,
