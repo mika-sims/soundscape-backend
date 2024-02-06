140298a18496e88263c9eb902b3bbff500357dd6
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-02-06 21:10:48 +0100

    Create Follower model

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Follower(models.Model):
+    """
+    Follower model for storing user following relationships.
+
+    The 'unique_together' is used to enforce a unique constraint 
+    on a combination of fields. This means that user can't follow
+    another user more than once.
+
+    owner = user who is following another user
+    followed = user who is followed by another user
+    related_name = to distinguish users.
+    """
+    owner = models.ForeignKey(
+        User, related_name='following', on_delete=models.CASCADE
+    )
+    followed = models.ForeignKey(
+        User, related_name='followed', on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'followed']
+
+    def __str__(self):
+        return f'{self.owner} {self.followed}'
