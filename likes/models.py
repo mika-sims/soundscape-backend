c288024bd2fa26082054e430dc7cea62854e6fae
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-02-06 15:01:08 +0100

    Create Like model

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from posts.models import Post
+
+
+class Like(models.Model):
+    """
+    Like model, related to Post and User models.
+    'owner' is the user who liked the post.
+    'post' is the post that was liked.
+    'unique_together' ensures that a user can only like a post once.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='likes', on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'post']
+
+    def __str__(self):
+        return f'{self.owner} {self.post}'
