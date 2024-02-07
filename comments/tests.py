a63eba47bb38e131cb1d59ee5a4b2d385c0357bc
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-02-07 21:37:06 +0100

    Write test for str method

--- comments/tests.py
+++ comments/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from rest_framework import status
+from rest_framework.test import APITestCase, APIRequestFactory
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import Comment
+from .serializers import CommentSerializer
+from posts.models import Post
+
+
+class CommentModelTest(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='testuser', password='password')
+
+    def test_str_method(self):
+        testuser = User.objects.get(username='testuser')
+        post = Post.objects.create(
+            owner=testuser,
+            content='test content',
+            latitude=123.456,
+            longitude=123.456,
+        )
+        comment = Comment.objects.create(
+            owner=testuser,
+            post=post,
+            content='test content'
+        )
+        self.assertEqual(str(comment), comment.content)
